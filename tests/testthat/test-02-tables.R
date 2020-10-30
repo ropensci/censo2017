e0640dbef956f6219c73ff9a74ba8fb2117a3d2e
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-10-30 12:30:55 -0300

    actualiza tipo de conexion en tests

--- tests/testthat/test-02-tables.R
+++ tests/testthat/test-02-tables.R
@@ -9,7 +9,7 @@ test_that("censo_datos returns tbl_df", {
   skip_on_cran()
   skip_if_not(censo_estado())
   
-  expect_is(censo_bbdd(), "duckdb_connection")
+  expect_is(censo_bbdd(), "SQLiteConnection")
   
   for (t in c("comunas", "regiones")) {
     expect_is(censo_datos(t), "tbl_df")
