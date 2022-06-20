da23b185ae2632be44dff282ed7a2a5819dd4629
Pachá <mavargas11@uc.cl>
2022-06-20 00:12:49 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   msg(" ")
   msg("La documentacion del paquete y ejemplos de uso se encuentran en https://pacha.dev/censo2017/.")
   msg("Visita https://buymeacoffee.com/pacha si deseas donar para contribuir al desarrollo de este software.")
-  msg("Este paquete necesita 3.5 GB libres para la crear la base de datos localmente.")
+  msg("Esta libreria necesita 3.5 GB libres para la crear la base de datos localmente. Una vez creada la base, esta ocupa 1.0 GB en disco.")
   msg(" ")
   if (interactive() && Sys.getenv("RSTUDIO") == "1"  && !in_chk()) {
     censo_pane()
